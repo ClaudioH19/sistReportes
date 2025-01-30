5e0cf1481c63baf0382d75c726373768a6c0e0e2
ClaudioH19 <clhernandez21@alumnos.utalca.cl>
2025-01-30 17:24:10 -0300

    version en gris

--- src/Components/paleta_colores.js
+++ src/Components/paleta_colores.js
@@ -1,47 +1,47 @@
 export const selectorbar_background_color = "#f9f9f9";
 //export const selectorbar_background_color = "#ffffff";
-export const button__hover_color = " #1b487f";
-export const button_color = "#0e3c78";
-export const table_header_color = "#3354A3";
+export const button__hover_color = "#4F4F4F";
+export const button_color = "#696969";
+export const table_header_color = "#808080";
 
 export const text_color = "#000000";
-export const axis_color = "#000000";
-export const split_line_color = "#89cfe1";
+export const axis_color = "#808080";
+export const split_line_color = "#A9A9A9";
 
 //rango bueno-intermedio-malo
 //export const kpis_color = ["#8cb3e0", "#3c6eb4", "#3354A3"];
 export const kpis_color = ["#4CAF50", "#FFC107", "#D32F2F"];
-export const barchart_color = "#3354A3";
+export const barchart_color = "#808080";
 
 // linea-item colors
-export const linechart_color = ["#5E77C4", "#A4B8E1"];
+export const linechart_color = ["#A9A9A9", "#C0C0C0"];
 
 //usada en rosquilla para los elementos
 export const donutchart_colors = [
-  "#3354A3",
-  "#1E3D89",
-  "#4F6FD1",
-  "#142B66",
-  "#6B89D1",
-  "#213D73",
-  "#89A3E6",
-  "#102654",
-  "#A4B8F1",
-  "#0D1D40",
-  "#C7DAFF",
-  "#08132B",
-  "#E0ECFF",
+  "#696969",
+  "#808080",
+  "#A9A9A9",
+  "#C0C0C0",
+  "#D3D3D3",
+  "#A0A0A0",
+  "#BEBEBE",
+  "#8F8F8F",
+  "#DCDCDC",
+  "#A8A8A8",
+  "#E0E0E0",
+  "#B5B5B5",
+  "#F5F5F5",
 ];
 
 //para representar el calor
 export const heatmapchart_colors = [
-  "#A4B8F1",
-  "#89A8F1",
-  "#7394E6",
-  "#5E7FD1",
-  "#4A6AC4",
-  "#3354A3",
-  "#1E3D89",
-  "#142B66",
-  "#0A1F50",
-];
+  "#D3D3D3",
+  "#C0C0C0",
+  "#A9A9A9",
+  "#808080",
+  "#696969",
+  "#585858",
+  "#484848",
+  "#383838",
+  "#282828",
+].reverse();
